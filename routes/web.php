e14d0779a8659a24d97415d70c4011523caa6e0f
sk <senthalan.sk@gamil.com>
2025-01-09 20:57:41 +0530

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::get('/redirect',[HomeController::class,'redirect']);
+Route::get('/',[HomeController::class,'index']);
